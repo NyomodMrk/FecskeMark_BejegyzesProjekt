e314961e06ca048c362bd0e54f7f4996e5dbd4b6
Fecske Márk <mrk.fecske@gmail.com>
2023-09-26 08:55:03 +0200

    2-es Feladat b részének elkészítése

--- BejegyzesProjekt/Program.cs
+++ BejegyzesProjekt/Program.cs
@@ -8,6 +8,31 @@ namespace BejegyzesProjekt
 {
     internal class Program
     {
+        List<Bejegyzes> lista= new List<Bejegyzes>();
+        List<Bejegyzes> lista2= new List<Bejegyzes>();
+
+        public void Feladat2_b()
+        {
+            Console.WriteLine("Kérem adja meg hány darab bejegyzést szeretne létrehozni! ");
+            int darab = int.Parse(Console.ReadLine());
+            if (darab < 0)
+            {
+                Console.WriteLine("Hiba, nem természetes számot adott meg");
+            }
+            else
+            {
+                for (int i = 0; i < darab; i++)
+                {
+                    Console.WriteLine("Kérem adja meg a szerzőt");
+                    string szerzo = Console.ReadLine();
+                    Console.WriteLine("Kérem adja meg a tartalmát");
+                    string tartalom = Console.ReadLine();
+                    DateTime dateTime = DateTime.Now;
+                    Bejegyzes bejegyz = new Bejegyzes(szerzo, tartalom, dateTime);
+                    lista.Add(bejegyz);
+                }
+            }
+        }
         static void Main(string[] args)
         {
         }
